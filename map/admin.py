c326c5b44c62e76e40d50a1de9dd321c75a85db6
8282peoplein <8282peoplein@gmail.com>
2019-12-03 14:21:17 +0900

    Changed ...
    Former-commit-id: c5d798db94afc558c7fb350ea6f623aeb5f4bc3f

--- map/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
-from .models import *
-
-# admin.site.register(item)
-# @admin.register(Seoul, Bucheon)
-# class ViewAdmin(ImportExportModelAdmin):
-#     exclude = ('id', )
